cf6e26cdd38d87b47109c933a52b0f9ffc2695fd
soltan2025 <s6219992@gmail.com>
2025-03-29 23:05:44 -0700

    add TripHandler

---
+++ TripHandler.java
@@ -0,0 +1,8 @@
+import java.math.BigDecimal;
+
+public class TripHandler {
+    public static BigDecimal getPrice(Point src, Point dest, Taxi taxi) {
+        return taxi.getPrice(src,dest);
+    }
+}
+
